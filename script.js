32cffe3dd4137475a8ec400adf45e13c77a94dde
Ashish Gaikwad <36022739+AshishGaikwad717@users.noreply.github.com>
2024-11-15 14:17:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let age=document.getElemetById('age');
+let name=document.getElementById('name');
+let promise=new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		if(age>18){
+			resolve(`welcome ${name}. You can vote.`);
+		}
+		else{
+			reject(`Oh sorry ${name}. You aren't old enough.`);
+		}
+	},4000);
+});
+promise.then((res)=>window.alert(res)).catch((rej)=>window.alert(rej));
